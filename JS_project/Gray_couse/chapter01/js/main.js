64a59cc71d92c70d7166b1fceb87b669c1c6151d
ms.jang <asomu34@gmail.com>
2022-11-02 08:06:02 +0900

    js 11/02

--- JS_project/Gray_couse/chapter01/js/main.js
+++ JS_project/Gray_couse/chapter01/js/main.js
@@ -1,4 +1,10 @@
-// String
-let ret = prompt("test");
-// let ret = confirm("Hello Wolrd");
-console.log(ret ?? "Nothing");
+const getUserName = (email) => {
+  return email.slice(0, email.indexOf("@"));
+};
+
+const toProperCase = (name) => {
+  return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+};
+
+console.log(getUserName("mark@gmail.com"));
+console.log(toProperCase("i WANNA Go Tolilet."));
