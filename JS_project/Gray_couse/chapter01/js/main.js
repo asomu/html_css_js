19eacb858ef0a17a6ca775b14c17ff21cf1ba248
ms.jang <asomu34@gmail.com>
2022-11-07 11:16:26 +0900

    AddEventListener

--- JS_project/Gray_couse/chapter01/js/main.js
+++ JS_project/Gray_couse/chapter01/js/main.js
@@ -1,72 +1,35 @@
 const view1 = document.getElementById("view1");
 // console.log(view1);
 
-const view2 = document.querySelector("#view2");
-// console.log(view2);
-view1.style.display = "flex";
+const view2 = document.querySelector(".view2");
+const div = view2.querySelector("div");
+const h2 = div.querySelector("h2");
+console.log(view2);
+
 view2.style.display = "flex";
 
-const views = document.getElementsByClassName("view");
-console.log(views);
-const sameViews = document.querySelectorAll(".view");
-console.log(sameViews);
+view2.addEventListener(
+  "click",
+  (evnet) => {
+    view2.classList.toggle("purple");
+    view2.classList.toggle("darkblue");
+    console.log("Add");
+  },
+  false
+);
 
-const divs = view1.querySelectorAll("div");
-console.log(divs);
+div.addEventListener(
+  "click",
+  (evnet) => {
+    evnet.target.style.backgroundColor = "blue";
+  },
+  false
+);
 
-const sameDivs = view1.getElementsByTagName("div");
-console.log(sameDivs);
-
-const evenDivs = view1.querySelectorAll("div:nth-of-type(2n");
-console.log(evenDivs);
-
-for (let i = 0; i < evenDivs.length; i++) {
-  evenDivs[i].style.backgroundColor = "darkblue";
-  // evenDivs[i].style.width = "200px";
-  // evenDivs[i].style.height = "200px";
-}
-
-const navText = document.querySelector("nav h1");
-console.log(navText);
-navText.textContent = "Hello world!";
-const navBar = document.querySelector("nav");
-navBar.innerHTML = `<h1>Hello!</h1> <p>This shoud align right.</p>`;
-console.log(navBar);
-
-console.log(evenDivs[0]);
-console.log(evenDivs[0].parentElement);
-console.log(evenDivs[0].parentElement.children);
-console.log(evenDivs[0].parentElement.childNodes);
-console.log(evenDivs[0].parentElement.hasChildNodes());
-console.log(evenDivs[0].parentElement.lastChild);
-console.log(evenDivs[0].parentElement.lastElementChild);
-console.log(evenDivs[0].parentElement.firstChild);
-console.log(evenDivs[0].parentElement.firstElementChild);
-console.log(evenDivs[0].nextElementSibling);
-console.log(evenDivs[0].previousElementSibling);
-
-view1.style.display = "none";
-view2.style.flexDirection = "row";
-view2.style.flexWrap = "wrap";
-view2.style.margin = "10px";
-
-while (view2.lastChild) {
-  view2.lastChild.remove();
-}
-
-const createDivs = (parent, iter) => {
-  const newDiv = document.createElement("div");
-  newDiv.textContent = iter;
-  newDiv.style.backgroundColor = "#000";
-  newDiv.style.width = "100px";
-  newDiv.style.height = "100px";
-  newDiv.style.margin = "10px";
-  newDiv.style.display = "flex";
-  newDiv.style.justifyContent = "center";
-  newDiv.style.alignItems = "center";
-  parent.append(newDiv);
-};
-
-for (let i = 1; i <= 12; i++) {
-  createDivs(view2, i);
-}
+h2.addEventListener(
+  "click",
+  (evnet) => {
+    evnet.target.textContent = "Clicked";
+  },
+  false
+);
