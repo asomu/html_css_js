57daf30938e5576ec304dc85c81b6f6c0a1088bc
ms.jang <asomu34@gmail.com>
2022-11-21 08:18:24 +0900

    react hooks in offce

---
+++ react_project/04tut/src copy/Footer.js
@@ -0,0 +1,13 @@
+import { isToday } from 'date-fns/esm';
+import React from 'react';
+
+const Footer = () => {
+  const today = new Date()
+  return (
+    <footer className='Footer'>
+      <p>Copyright &copy; {today.getFullYear()}</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ react_project/challenge/src/hooks/useWindowSize.js
@@ -0,0 +1,8 @@
+import { useState, useEffect } from 'react'
+
+const useWindowSize = () => {
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined
+    })
+}
